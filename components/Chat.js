fec62b1778f85988f13bd311bb52ed749d384fea
GabCB <gabycuencas@gmail.com>
2023-05-01 21:08:51 +0200

    5.2 implemented chat UI

--- components/Chat.js
+++ components/Chat.js
@@ -1,16 +1,71 @@
-import { useEffect } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { useState, useEffect } from 'react';
+import { KeyboardAvoidingView, Platform, StyleSheet, View } from 'react-native';
+//Import Gifted Chat library
+import { Bubble, GiftedChat } from 'react-native-gifted-chat';
 
 const Chat = ({ route, navigation }) => {
   const { name, color } = route.params;
 
+  //State initialization
+  const [messages, setMessages] = useState([]);
+
+  //Set the state with a static message
   useEffect(() => {
     navigation.setOptions({ title: name });
   }, []);
 
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: "Hello Gab",
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: "React Native",
+          avatar: "https://placeimg.com/140/140/any",
+        },
+      },
+      {
+        _id: 2,
+        text: name + "  you've entered the chat",
+        createdAt: new Date(),
+        system: true,
+      },
+    ]);
+  }, []);
+
+  //Custom function onSend() called when a user sends a message
+  const onSend = (newMessages) => {
+    setMessages(previousMessages => GiftedChat.append(previousMessages, newMessages))
+  }
+
+  const renderBubble = (props) => {
+    return <Bubble {...props}
+    wrapperStyle={{
+      right: {
+        backgroundColor: "#000"
+      },
+      left: {
+        backgroundColor: "#FFF"
+      }
+    }} />
+  }
+
   return (
     <View style={[styles.container, { backgroundColor: color }]}>
-      <Text>Chat</Text>
+      <GiftedChat
+      messages={messages}
+      renderBubble={renderBubble}
+      onSend={messages => onSend(messages)}
+      user={{
+        _id: 1
+      }}
+    />
+    {/*Fix keyboard hides the message input field on Android*/}
+    { Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null }
+    {/*Fix keyboard hides the message input field on iOS*/}
+    { Platform.OS === "ios" ? <KeyboardAvoidingView behavior="padding" /> : null }
     </View>
   );
 }
@@ -18,8 +73,6 @@ const Chat = ({ route, navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
   }
 });
 
